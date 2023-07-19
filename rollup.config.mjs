b30433cbf127696a286eb34f0b3d1d3741666e6a
22maksim <15maksim@mail.ru>
2023-07-19 18:04:42 +0300

    add modules rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,14 +1,35 @@
-import json from '@rollup/plugin-json';
+import json from "@rollup/plugin-json";
+import styles from "rollup-plugin-styles";
+import image from '@rollup/plugin-image';
+import { babel } from '@rollup/plugin-babel';
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
+
 
 export default {
-	input: './src/index.js',
+	input: "./src/index.js",
 	output: {
 		
-		file: './build/bundle.js',
-		format: 'cjs'
+		file: "./build/bundle.js",
+		format: "cjs"
 	},
-	plugins: [json()],
+	plugins: [
+		json(),
+		styles(),
+		image(),
+		babel({ 
+			babelHelpers: 'bundled',
+			presets: ["@babel/env"] 
+		}),
+		serve({
+			open: true,
+			contentBase: './',
+			port: 8000,
+		}),
+		livereload()
+	],
   watch: {
-    exclude: 'node_modules/**'
+    exclude: "node_modules/**"
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
+import '../index.css';
+import img from '../assets/tiger.jpg';
 
 
 
 console.log('Hello World!');
-document.body.innerHTML = '<p>Ты красавчик</p>';
+const pCreat = document.createElement('h1');
+pCreat.textContent = 'I love JavaScript';
+document.body.append(pCreat);
+const picture = document.createElement('img');
+picture.src = img;
+picture.classList.add('img-tiger');
+document.body.append(picture);
